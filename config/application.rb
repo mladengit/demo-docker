6f03aa20dc5ed5f2ab6cbd869c274c9f5d22919c
Mladen Raev <mladen.raev@asteasolutions.com>
2016-12-08 09:37:58 +0200

    add task for many jobs

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module DemoDocker
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :sidekiq
+    config.web_console.whiny_requests = false
     config.logger = Logger.new(STDOUT)
   end
 end
--- lib/tasks/demo.rake
+++ lib/tasks/demo.rake
@@ -3,4 +3,11 @@ namespace :demo do
   task run_demo_job: [:environment] do
     DemoJob.perform_later
   end
+
+  desc 'Run may jobs'
+  task run_many_jobs: [:environment] do
+    100.times do
+      DemoJob.perform_later
+    end
+  end
 end
